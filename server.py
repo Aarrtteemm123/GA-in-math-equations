48fee7dadff0e405b86bcdd49e8ccbe9757c84f9
Artem <aarrtteemm258013@gmail.com>
2022-10-17 18:30:33 +0300

    add form for input ga params

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.get('/')
-def main_page():
+@app.get('/',)
+def get_main_page():
+    return render_template('index.html')
+
+
+@app.route('/process_data', methods=['POST'])
+def process_data():
+    print(request.form)
     return render_template('index.html')
 
 
