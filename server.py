aec2d2cd2c0b2390b0ccb0da9b598b96a4e40291
Artem <aarrtteemm258013@gmail.com>
2022-10-17 19:20:07 +0300

    add page for display result

--- server.py
+++ server.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 
-
-@app.get('/',)
+@app.get('/', )
 def get_main_page():
     return render_template('index.html')
+
+
+@app.get('/result')
+def get_result_page():
+    data = session['result']
+    print(data)
+    return render_template('result.html', data=data)
 
 
 @app.route('/process_data', methods=['POST'])
 def process_data():
     print(request.form)
-    return render_template('index.html')
+    data = {'result': 45}
+    session['result'] = data
+    return redirect(url_for('get_result_page'))
 
 
 if __name__ == '__main__':
