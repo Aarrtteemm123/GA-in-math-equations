077f9b6ce0193d054fc30b46f8f542e00953efe0
Artem <aarrtteemm258013@gmail.com>
2022-11-10 20:19:00 +0200

    added AnalyzerComputerAlgebra

--- statistic.py
+++ statistic.py
@@ -1,10 +1,14 @@
 import json
 
+import sympy
+from sympy import Symbol, solve, Pow, exp, sin, cos, tan
+
 from config import Config
 from ga_interface import GAInterface
+from tools import benchmark
 
 
-class Analyser:
+class AnalyserGA:
     def __init__(self, path_to_result, num_loop):
         self.path_to_result = path_to_result
         self.num_loop = num_loop
@@ -81,13 +85,13 @@ class Analyser:
         self.result['polynomial_5'] = result
 
     def exponential_equation(self, a, b, c):
-        equation = f'{a}*(x**{b})+{c}'
+        equation = f'{a}*(math.e**(x*{b}))+{c}'
         ga = GAInterface(equation)
         ga.build_solver(num_generations=1000,
                         num_parents_mating=2,
-                        sol_per_pop=10,
+                        sol_per_pop=20,
                         num_genes=10,
-                        accuracy=0.1,
+                        accuracy=0.01,
                         crossover_type='two_points',
                         mutation_probability=0.15,
                         parallel_processing=1)
@@ -151,7 +155,7 @@ class Analyser:
         self.result['ctg_x'] = result
 
     def save(self):
-        with open(self.path_to_result+'result.json', 'w') as file:
+        with open(self.path_to_result + 'general_result.json', 'w') as file:
             file.write(json.dumps(self.result, indent=4))
 
     def _run(self, ga):
@@ -182,19 +186,76 @@ class Analyser:
         }
 
 
+class AnalyzerComputerAlgebra:
+    def __init__(self):
+        self.x = Symbol('x')
+
+    @benchmark
+    def linear_equation(self, a, b):
+        res_lst = solve(a * self.x + b, self.x)
+        return [float(res) for res in res_lst]
+
+    @benchmark
+    def polynomial_2(self, a, b, c):
+        res_lst = solve(a * Pow(self.x, 2) + b * self.x + c, self.x)
+        return [float(res) for res in res_lst]
+
+    @benchmark
+    def polynomial_3(self, a, b, c, d):
+        res_lst = solve(a * Pow(self.x, 3) + b * Pow(self.x, 2) + c * self.x + d, self.x)
+        return [float(res) for res in res_lst]
+
+    @benchmark
+    def polynomial_4(self, a, b, c, d, e):
+        res_lst = solve(a * Pow(self.x, 4) + b * Pow(self.x, 3) + c * Pow(self.x, 2) + d * self.x + e, self.x)
+        return [float(res) for res in res_lst]
+
+    @benchmark
+    def polynomial_5(self, a, b, c, d, e, f):
+        res_lst = solve(a * Pow(self.x, 5) + b * Pow(self.x, 4) + c * Pow(self.x, 3) + d * Pow(self.x, 2) + e * self.x + f, self.x)
+        return [float(res) for res in res_lst]
+
+    @benchmark
+    def exponential_equation(self, a, b, c):
+        res_lst = solve(a * exp(b * self.x) + c, self.x)
+        return [float(res) for res in res_lst]
+
+    @benchmark
+    def sin_x(self, a, b, c):
+        res_lst = solve(a * sin(b * self.x) + c, self.x)
+        return [float(res) for res in res_lst]
+
+    @benchmark
+    def cos_x(self, a, b, c):
+        res_lst = solve(a * cos(b * self.x) + c, self.x)
+        return [float(res) for res in res_lst]
+
+    @benchmark
+    def tg_x(self, a, b, c):
+        res_lst = solve(a * tan(b * self.x) + c, self.x)
+        return [float(res) for res in res_lst]
+
+    @benchmark
+    def ctg_x(self, a, b, c):
+        res_lst = solve(a / tan(b * self.x) + c, self.x)
+        return [float(res) for res in res_lst]
+
+
 def run_statistic():
-    analyser = Analyser(Config.PATH_TO_STATISTIC, Config.NUM_LOOPS)
-    analyser.linear_equation(5, -3)
+    analyser = AnalyserGA(Config.PATH_TO_STATISTIC, Config.NUM_LOOPS)
+    anal_comp_alg = AnalyzerComputerAlgebra()
+    print(anal_comp_alg.polynomial_2(4, 2, -5))
+    # analyser.linear_equation(5, -3)
     # analyser.polynomial_2(2, 5, -15)
     # analyser.polynomial_3(4, 2, 7, -5)
-    # analyser.polynomial_4(7, -2, -6, -3, 10)
+    # analyser.polynomial_4(-4, -7, 5, 5, 1)
     # analyser.polynomial_5(-3, -4, -7, 5, 5, 1)
-    # analyser.exponential_equation(4, 3, 2)
+    # analyser.exponential_equation(5, 1, -10)
     # analyser.sin_x(4, 2, 2)
     # analyser.cos_x(7, 1, 1)
     # analyser.tg_x(-5, 3, -2)
-    # analyser.ctg_x(1, 1, 1)
-    analyser.save()
+    # analyser.ctg_x(4, 8, -10)
+    #analyser.save()
 
 
 if __name__ == '__main__':
--- tools.py
+++ tools.py
@@ -4,10 +4,10 @@ import time
 def benchmark(func):
     def wrapper(*args, **kwargs):
         start = time.time()
-        func(*args, **kwargs)
+        res = func(*args, **kwargs)
         finish = time.time()
         execution_time = finish - start
-        print('Execution time: ', execution_time)
-        return execution_time
+        #print('Execution time: ', execution_time)
+        return {'execution_time': execution_time, 'func_res': res}
     return wrapper
 
